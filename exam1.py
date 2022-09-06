b503c60203e5b193049425532a1123cc701f4480
hyeonwook <yho7955@naver.com>
2022-09-06 22:38:13 +0900

    exam3.py
    이코테다 - 큰수의법칙

--- exam1.py
+++ exam1.py
@@ -1,12 +1,7 @@
-n = 1260
-count = 0
-
-coin_list= [500,100,50,10] # 큰 단위 화페부터 작은 단위 화페를 리스트에 담고
-
-for i in coin_list:
-    count += n//i # 해당 화폐로 거슬러 줄 수 있는 동전의 개수 세기 , n/i 라는뜻
-    n%= i
-
-print(count)
-
-# 정답 ; 6
+a,b = map(int,input().split())
+if a > b:
+    print('>')
+elif a < b:
+    print('<')
+else:
+    print('==')
